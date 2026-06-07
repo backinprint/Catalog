3e5f0ecd94cc079f5613d0078d00f2164e7dd029
upt0wn112 <lizway4@gmail.com>
2026-06-07 18:39:45 -0400

    Add product prices

--- public/js/products-data.js
+++ public/js/products-data.js
@@ -2,41 +2,41 @@ window.PRODUCT_DATA = {
   "C0004_Cowboy_Frog.jpg": {
     "itemName": "Cowboy Frog",
     "itemNumber": "C0004",
-    "itemPrice": ""
+    "itemPrice": "15"
   },
   "C001_Hammerhead.jpg": {
     "itemName": "Hammerhead",
     "itemNumber": "C001",
-    "itemPrice": ""
+    "itemPrice": "10"
   },
   "C003_Cowboy_Crab.jpg": {
     "itemName": "Cowboy Crab",
     "itemNumber": "C003",
-    "itemPrice": ""
+    "itemPrice": "25"
   },
   "C018_Gummy_Bear.jpg": {
     "itemName": "Gummy Bear",
     "itemNumber": "C018",
-    "itemPrice": ""
+    "itemPrice": "1"
   },
   "C103_Fire_Dragon_Hatchling_Multi.jpg": {
     "itemName": "Fire Dragon Hatchling Multi",
     "itemNumber": "C103",
-    "itemPrice": ""
+    "itemPrice": "5"
   },
   "C103_Fire_Dragon_Hatchling.jpg": {
     "itemName": "Fire Dragon Hatchling",
     "itemNumber": "C103",
-    "itemPrice": ""
+    "itemPrice": "2"
   },
   "C107_Crystal_Tadling.jpg": {
     "itemName": "Crystal Tadling",
     "itemNumber": "C107",
-    "itemPrice": ""
+    "itemPrice": "3"
   },
   "C113_Phoenix_Dragon.jpg": {
     "itemName": "Phoenix Dragon",
     "itemNumber": "C113",
-    "itemPrice": ""
+    "itemPrice": "8"
   }
 };
