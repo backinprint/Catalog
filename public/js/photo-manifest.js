36f189b31407567ca6f2e7db9ff2097a17b3970f
upt0wn112 <lizway4@gmail.com>
2026-06-16 09:43:36 -0400

    Add new product photos

--- public/js/photo-manifest.js
+++ public/js/photo-manifest.js
@@ -1,11 +1,20 @@
 ﻿window.PRODUCT_PHOTOS = [
   "C0004_Cowboy_Frog.jpg",
   "C001_Hammerhead.jpg",
+  "C002M_Lizard_Hatchling.jpg",
   "C003_Cowboy_Crab.jpg",
+  "C012_Quackodile.jpg",
   "C018_Gummy_Bear.jpg",
+  "C024_Guinea_Pig.jpg",
+  "C025_Ice_Cream_Narwhal.jpg",
   "C103_Fire_Dragon_Hatchling.jpg",
-  "C103_Fire_Dragon_Hatchling_Multi.jpg",
+  "C103_Fire_Dragon_Hatchling_v2.jpg",
+  "C103M_Fire_Dragon_Hatchling_Multi.jpg",
+  "C105_80M_Small_Baby_Gemstone_Multi.jpg",
   "C107_Crystal_Tadling.jpg",
-  "C113_Phoenix_Dragon.jpg"
+  "C108_Crystal_Dragon.jpg",
+  "C113_Phoenix_Dragon.jpg",
+  "K001_World_Cup_Trophy_Clicker.jpg",
+  "X001_Double_Clicker.jpg"
 ];
 
--- public/js/products-data.js
+++ public/js/products-data.js
@@ -1,4 +1,4 @@
-window.PRODUCT_DATA = {
+﻿window.PRODUCT_DATA = {
   "C0004_Cowboy_Frog.jpg": {
     "itemName": "Cowboy Frog",
     "itemNumber": "C0004",
@@ -9,34 +9,79 @@ window.PRODUCT_DATA = {
     "itemNumber": "C001",
     "itemPrice": "10"
   },
+  "C002M_Lizard_Hatchling.jpg": {
+    "itemName": "Lizard Hatchling",
+    "itemNumber": "C002M",
+    "itemPrice": "2"
+  },
   "C003_Cowboy_Crab.jpg": {
     "itemName": "Cowboy Crab",
     "itemNumber": "C003",
     "itemPrice": "25"
+  },
+  "C012_Quackodile.jpg": {
+    "itemName": "Quackodile",
+    "itemNumber": "C012",
+    "itemPrice": "8"
   },
   "C018_Gummy_Bear.jpg": {
     "itemName": "Gummy Bear",
     "itemNumber": "C018",
     "itemPrice": "1"
   },
-  "C103_Fire_Dragon_Hatchling_Multi.jpg": {
-    "itemName": "Fire Dragon Hatchling Multi",
-    "itemNumber": "C103",
-    "itemPrice": "5"
+  "C024_Guinea_Pig.jpg": {
+    "itemName": "Guinea Pig",
+    "itemNumber": "C024",
+    "itemPrice": "2"
+  },
+  "C025_Ice_Cream_Narwhal.jpg": {
+    "itemName": "Ice Cream Narwhal",
+    "itemNumber": "C025",
+    "itemPrice": "2"
   },
   "C103_Fire_Dragon_Hatchling.jpg": {
     "itemName": "Fire Dragon Hatchling",
     "itemNumber": "C103",
     "itemPrice": "2"
   },
+  "C103_Fire_Dragon_Hatchling_v2.jpg": {
+    "itemName": "Fire Dragon Hatchling v2",
+    "itemNumber": "C103",
+    "itemPrice": "2"
+  },
+  "C103M_Fire_Dragon_Hatchling_Multi.jpg": {
+    "itemName": "Fire Dragon Hatchling Multi",
+    "itemNumber": "C103M",
+    "itemPrice": "5"
+  },
+  "C105_80M_Small_Baby_Gemstone_Multi.jpg": {
+    "itemName": "Small Baby Gemstone Multi",
+    "itemNumber": "C105",
+    "itemPrice": "15"
+  },
   "C107_Crystal_Tadling.jpg": {
     "itemName": "Crystal Tadling",
     "itemNumber": "C107",
     "itemPrice": "3"
   },
+  "C108_Crystal_Dragon.jpg": {
+    "itemName": "Crystal Dragon",
+    "itemNumber": "C108",
+    "itemPrice": "25"
+  },
   "C113_Phoenix_Dragon.jpg": {
     "itemName": "Phoenix Dragon",
     "itemNumber": "C113",
     "itemPrice": "8"
+  },
+  "K001_World_Cup_Trophy_Clicker.jpg": {
+    "itemName": "World Cup Trophy Clicker",
+    "itemNumber": "K001",
+    "itemPrice": "3"
+  },
+  "X001_Double_Clicker.jpg": {
+    "itemName": "Double Clicker",
+    "itemNumber": "X001",
+    "itemPrice": "1.50"
   }
 };
